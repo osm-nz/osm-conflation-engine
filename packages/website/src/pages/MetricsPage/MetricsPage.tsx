7ed581db8e52f769177d334662d5bad1b6450242
Kyle Hensel <k.y.l.e@outlook.co.nz>
2026-09-24 17:39:00 +0800

    chore: some unavailable metrics when using postpass

--- packages/website/src/pages/MetricsPage/MetricsPage.tsx
+++ packages/website/src/pages/MetricsPage/MetricsPage.tsx
@@ -1,25 +1,33 @@
 import { use } from 'react';
 import { Flex } from '@mantine/core';
+import TimeAgo from 'react-timeago-i18n';
 import { useProject } from '../../hooks/useProject.js';
 import { FullPageLoading } from '../../components/FullPageLoading.js';
 import { PageNotFound } from '../../components/PageNotFound.js';
+import { OidcBadge } from '../../components/OidcBadge.js';
 import { LocaleContext } from '../../context/LocaleContext.js';
+import { OsmTag } from '../../components/OsmTag.js';
 import { Sankey } from './Sankey.js';
 import { Venn } from './Venn.js';
 import { ConflationResult } from './ConflationResult.js';
 import { IgnoredByUser } from './IgnoredByUser.js';
 import { IgnoredByReview } from './IgnoredByReview.js';
-import { StatCard } from './StatCard.js';
+import { GenericCard, StatCard } from './StatCard.js';
 
 export const MetricsPage: React.FC = () => {
   const { $, $$ } = use(LocaleContext);
-  const { metrics, ignoreList, notFound } = useProject();
+  const { project, metrics, ignoreList, notFound } = useProject();
 
   if (notFound) return <PageNotFound />;
-  if (!metrics) return <FullPageLoading />;
+  if (!metrics || !project) return <FullPageLoading />;
 
   const { osm } = metrics.countsByPhase.init;
   const totalOsm = osm.withRef + osm.duplicateRefs + osm.semi + osm.noRef;
+
+  const disabledMessage =
+    metrics.config.o_data.source.type === 'postpass'
+      ? $$('MetricsPage.postpass_unavailable')
+      : undefined;
 
   return (
     <Flex wrap="wrap" gap="md" align="stretch">
@@ -31,21 +39,42 @@ export const MetricsPage: React.FC = () => {
       <IgnoredByReview ignoreList={ignoreList} />
 
       <StatCard
-        label={$$('MetricsPage.recent_check_date', {
-          key: metrics.config.o_data.check_date_key || 'check_date',
-        })}
+        label={$$(
+          'MetricsPage.recent_check_date',
+          {},
+          {
+            osmKey: () => (
+              <OsmTag
+                tag={metrics.config.o_data.check_date_key || 'check_date'}
+              />
+            ),
+          },
+        )}
         count={osm.recentlyChecked}
         total={totalOsm}
+        disabledMessage={disabledMessage}
       />
       <StatCard
         label={$('MetricsPage.recently_modified')}
         count={osm.recentlyChanged}
         total={totalOsm}
+        disabledMessage={disabledMessage}
       />
       <StatCard
         label={$('MetricsPage.last_edited_by_importer')}
         count={osm.lastEditedByImporter}
         total={totalOsm}
+        disabledMessage={disabledMessage}
+      />
+      <GenericCard
+        label={$('MetricsPage.last_updated')}
+        title={<TimeAgo date={project.timestamp} />}
+        subtitle={
+          <OidcBadge
+            operator={project.operator}
+            timestamp={project.timestamp}
+          />
+        }
       />
     </Flex>
   );
--- packages/website/src/pages/MetricsPage/StatCard.tsx
+++ packages/website/src/pages/MetricsPage/StatCard.tsx
@@ -3,13 +3,11 @@ import { Card, Stack, Text } from '@mantine/core';
 import { LocaleContext } from '../../context/LocaleContext.js';
 import classes from './MetricsPage.module.css';
 
-export const StatCard: React.FC<{
+export const GenericCard: React.FC<{
   label: React.ReactNode;
-  count: number;
-  total: number;
-}> = ({ label, count, total }) => {
-  const { $, locale } = use(LocaleContext);
-
+  title: React.ReactNode;
+  subtitle: React.ReactNode;
+}> = ({ label, title, subtitle }) => {
   return (
     <Card withBorder padding="xs" bg="#fcfcfb" className={classes.statCard}>
       <Stack gap={2} p={4}>
@@ -17,17 +15,40 @@ export const StatCard: React.FC<{
           {label}
         </Text>
         <Text fw={600} fz={40} lh={1.1}>
-          {total
-            ? (count / total).toLocaleString(locale, {
-                style: 'percent',
-                maximumFractionDigits: 1,
-              })
-            : '–'}
+          {title}
         </Text>
         <Text size="xs" c="dimmed">
-          {$('Common.count_of_total', { count, total })}
+          {subtitle}
         </Text>
       </Stack>
     </Card>
+  );
+};
+
+export const StatCard: React.FC<{
+  label: React.ReactNode;
+  count: number;
+  total: number;
+  disabledMessage?: React.ReactNode;
+}> = ({ label, count, total, disabledMessage }) => {
+  const { $, locale } = use(LocaleContext);
+
+  if (disabledMessage) {
+    return <GenericCard label={label} title="?" subtitle={disabledMessage} />;
+  }
+
+  const title = total
+    ? (count / total).toLocaleString(locale, {
+        style: 'percent',
+        maximumFractionDigits: 1,
+      })
+    : '?';
+
+  return (
+    <GenericCard
+      label={label}
+      title={title}
+      subtitle={$('Common.count_of_total', { count, total })}
+    />
   );
 };
